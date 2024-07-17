d55c6cc635c780bc46a30afa646412942e36a592
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-07-17 16:26:39 +0000

    trimmed

--- src/main/java/org/springboot/ecom/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springboot.ecom.repositories;
-
-import org.springboot.ecom.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
